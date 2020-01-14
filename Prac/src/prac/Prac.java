0ef9358f14a210aeb587b96857f6f182e7c4e520
Matthew Fischer <mattfisc@gmail.com>
2020-01-14 08:58:43 -0800

    prac program languages

---
+++ Prac/src/prac/Prac.java
@@ -0,0 +1,22 @@
+/*
+ * By:     Matthew Fischer
+ * Date:   
+ */
+package prac;
+
+/**
+ *
+ * @author Matthew Fischer
+ */
+public class Prac {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double x = 1/3;
+        
+        System.out.println(x);
+    }
+    
+}
