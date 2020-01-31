f319100ae641985144d53d71bd15ed1b04ecd94b
Matthew Fischer <mattfisc@gmail.com>
2020-01-31 00:33:53 -0800

    assignment2

--- Assignment3/src/assignment3/Main.java
+++ Assignment3/src/assignment3/Main.java
@@ -9,23 +9,27 @@ import java.util.Scanner;
 
 //00	r1
 //01	r2
-//02	add		(addressA + addressB and saved in second address)
-//03	sub		(addressA - addressB and saved in second address)
-//04	multiply	(addressA * addressB and saved in second address)
-//05	divide		(addressA / addressB and saved in second address)
-//06	load instance (load value to address)
-//07	load from memory (load memory to address)
-//08	save from register to memory
-//09	quit program
-//10	square number	(addressA * addressA and saved in addressA)
-
 public class Main {
 
     
     public static void main(String[] args) {
         Scanner x;
+        
         // READ FILE
         File file = new File("opcode.txt");
+        
+        // REGISTER 1
+        String register1 = "";
+
+        // REGISTER 2
+        String  register2 = "";
+
+        int rA = 0;
+        int rB = 0;
+
+        int memory1 = 0;
+        int memory2 = 0;
+        
         
         // File try catch
         try{
@@ -35,49 +39,133 @@ public class Main {
             while(x.hasNext()){
                 // OPCODE
                 String op = x.next();
-                char sign = ' ';
-                
+                String sign = op;
+                // END PROGRAM
+                if(op.equals("00")){
+                    break;
+                }
                 // REGISTER 1
-                String r1String = x.next();
-                int rA = Integer.parseInt(r1String);
+                register1 = x.next();
+                
                 
                 // REGISTER 2
-                String r2String = x.next();
-                int rB = Integer.parseInt(r2String);
+                register2 = x.next();
                 
-                int temp = 0;
-                
+               
                 // OPCODE
                 switch(op){
+                    //02 add (addressA + addressB and saved in second address)
                     case "02":
-                        sign = '+';
+                        // REGISTER IS 000
+                        if(register2.equals("000")){
+                            rA = rA + rB;
+                        }
+                        else{
+                            rB = rB + rA;
+                        }
                         break;
+                    //03 sub (addressA - addressB and saved in second address)
                     case "03":
-                        sign = '-';
+                        // REGISTER IS 000
+                        if(register2.equals("000")){
+                            rA = rB - rA;
+                        }
+                        else{
+                            rB = rA - rB;
+                        }
                         break;
+                    //04 multiply (addressA * addressB and saved in second address)
                     case "04":
-                        sign = '*';
+                        // REGISTER IS 000
+                        if(register2.equals("000")){
+                            rA = rA * rB;
+                        }
+                        else{
+                            rB = rB * rA;
+                        }
                         break;
+                    //05 divide (addressA / addressB and saved in second address)
                     case "05":
-                        sign = '/';
+                        // REGISTER IS 000
+                        if(register2.equals("000")){
+                            rA = rB / rA;
+                        }
+                        else{
+                            rB = rA / rB;
+                        }
                         break;
-                    default:
-                        System.out.println("sign error");
-                }       
-                
-                switch(op){
+                    //06 load instance (load value to address)
                     case "06":
-                        rB = arithmetic(sign,rA,rB);
+                        // REGISTER IS 000
+                        if(register1.equals("000")){
+                            rA = Integer.parseInt(register2);
+                        }
+                        else{
+                            rB = Integer.parseInt(register2);
+                        }
                         break;
+                    //07 load from memory (load memory to address)
                     case "07":
+                        if(register1.equals("000") && register2.equals("400")){
+                            rA = memory1;
+                        }
+                        else if(register1.equals("000") && register2.equals("500")){
+                            rA = memory2;
+                        }
+                        else if(register1.equals("001") && register2.equals("400")){
+                            rB = memory1;
+                        }
+                        else{
+                            rB = memory2;
+                        }
                         break;
+                    //08 save from register to memory
                     case "08":
+                        if(register1.equals("000") && register2.equals("400")){
+                            memory1 = rA;
+                        }
+                        else if(register1.equals("001") && register2.equals("400")){
+                            memory1 = rB;
+                        }
+                        else if(register1.equals("000") && register2.equals("500")){
+                            memory2 = rA;
+                        }
+                        else{
+                            memory2 = rB;
+                        }
                         break;
                     case "09":
+                        // ENDS PROGRAM
                         break;
+                    //10 square number	(addressA * addressA and saved in addressA)
                     case "10":
+                        if(register1.equals("000")){
+                            rA = rA * rA;
+                        }
+                        else{
+                            rB = rB * rB;
+                        }
                         break;
+                    // OPCODE ABOVE 10
+                    default:
+                        System.out.println("opcode error");
                 }
+                
+                // DISPLAY OUTPUT
+                System.out.println("opcode\tr1 \tr2");
+                System.out.println(op + "\t" + register1 + "\t" + register2);        
+                System.out.println(sign + "\t" + rA + "\t" + rB + "\n");
+                
+                
+                
+            }
+            
+            // ANSWER IS STORED IN SECOND REGISTER
+            if(register2.equals("000")){
+                System.out.println("answer" + rA);
+            }
+            else{
+                System.out.println("answer" + rB);
             }
             
         }
@@ -87,8 +175,4 @@ public class Main {
         
     }
    
-    public int arithmetic(sign,rA,rB){
-        
-        return rB;
-    }
 }
