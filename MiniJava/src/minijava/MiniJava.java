518485d1747febd1cf646cd87754b7c73a9a288c
Matthew Fischer <mattfisc@gmail.com>
2020-04-03 14:22:33 -0700

    final project outline

---
+++ MiniJava/src/minijava/MiniJava.java
@@ -0,0 +1,18 @@
+/*
+ * By:     Matthew Fischer
+ * Date:   
+ */
+package minijava;
+
+/**
+ *
+ * @author Matthew Fischer
+ */
+public class MiniJava {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
